79291799f2eff56246d163f2bc7497cae5c44ea8
Alex Zaks <zaksa86@gmail.com>
2022-01-11 11:55:34 -0500

    need to display server

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,29 +1,13 @@
 import React, { useState } from "react";
 
 function Counter() {
-    const [counter1, setCounter1] = useState(0);
-    const [counter2, setCounter2] = useState(0);
-    const [counter3, setCounter3] = useState(0);
+    const [counter, setCounter1] = useState(0);
 
     return (
-        <div className="3counters">
-            <div className="Counter1">
+        <div className="Counter">
                 <button id="up" onClick={() => setCounter1((count) => count + 1)}>Up</button>
-            <p>{{counter1}}</p>
+            <p>{{counter}}</p>
                <button id="down" onClick={() => setCounter1((count) => count - 1)}>Up</button>
-            </div>
-
-            <div className="Counter2">
-                <button id="up" onClick={() => setCounter2((count) => count + 1)}>Up</button>
-            <p>{{counter2}}</p>
-               <button id="down" onClick={() => setCounter2((count) => count - 1)}>Up</button>
-            </div>
-
-            <div className="Counter3">
-                <button id="up" onClick={() => setCounter3((count) => count + 1)}>Up</button>
-            <p>{{counter3}}</p>
-               <button id="down" onClick={() => setCounter3((count) => count - 1)}>Up</button>
-            </div>
         </div>
     )
 }
